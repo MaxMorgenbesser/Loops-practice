e0213f5b1239aab4352b11ea5af50017700c84a0
Max Morgenbesser <maxwellmorgenbesser@gmail.com>
2022-07-14 16:37:46 -0400

    practicing loops

---
+++ loops.js
@@ -0,0 +1,76 @@
+// const array = [1, 3, 4, 2, 6, 7, 8, 4, 5, 3, 4];
+// // for (let i = 0; i < array.length; i++) {
+// //   console.log(array[i]);
+// // }
+// // for (let i = array.length-1; i >-1; i--){
+// //     console.log(array[i])
+// // }
+// // sum =0
+// // for (let i =0; i < array.length; i ++){
+// // sum += array[i]
+// // }
+// // console.log(sum)
+// for (let i = 10; i >= 0; i--) {
+//   console.log(i);
+// }
+// console.log("Blast Off");
+ 
+// let i =0 
+// while (i<10){
+//     i++
+//     console.log(i)
+// }
+
+// const obj = { a:1,b:2,c:3}
+// for (const key in obj){
+//     console.log(key)
+// }
+// const object={
+//     name:'max',
+//     age:22,
+//     inClass:true
+// }
+// for (const ke in object){
+//     console.log(ke)
+// }
+// for (const ke in object){
+//     console.log(object[ke])
+// }
+// for (value of array){
+//     statement;
+// }
+// arr=[10,20,30]
+// for (const value of arr ){
+//     console.log(value)
+// }
+// const nameArray =['Matt','tom','jack']
+// for (const value of nameArray){
+//     console.log(value)
+//     if (value==='Matt'){
+//         break
+//     }
+// }
+// for (let i=0;i<15;i++){
+//     if (i===12){
+//         continue
+//     }
+//     console.log(i)
+// }
+
+
+// for (let i=0; i<101; i=2+i){
+//     console.log(i)
+//  }
+
+
+  let namesArr=['jack','max','alex']
+  for (let i=0; i<namesArr.length; i++){
+    console.log(namesArr[i])
+ }
+ for (let i =namesArr.length; i>-1; i--){
+    console.log(namesArr[i])
+ }
+// let str='';  
+// for (let i = 0; i<=5; i++){
+//     str+='*'
+//  console.log(str)}
